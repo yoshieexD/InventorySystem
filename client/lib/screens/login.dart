31dcabac2d266160214cc57e81e28853417e3f4c
Jom-Winston <bjomwinston0107@gmail.com>
2023-08-07 11:36:19 +0800

    login client

---
+++ client/lib/screens/login.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login(BuildContext context) {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username == "username" && password == "password") {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Invalid username or password')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () => _login(context),
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- server/app.py
+++ server/app.py
@@ -16,21 +16,60 @@ username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
 
-def is_odoo_connected():
-    try:
-        common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
-        uid = common.authenticate(db, username, password, {})
-        return True
-    except xmlrpc.client.Fault as e:
-        return False
+def get_odoo_server():
+    common = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/common")
+    uid = common.authenticate(db, username, password, {})
+
+    if uid:
+        return xmlrpc.client.ServerProxy(
+            f"{url}/xmlrpc/2/object", allow_none=True
+        ), int(uid)
+
+    return None, None
 
 
 @app.route("/")
 def home():
-    if is_odoo_connected():
-        return "Hello World From Flask! Odoo is connected."
+    return "Hello World, from Flask! Odoo is connected."
+
+
+@app.route("/list_partners")
+def list_partners():
+    server, uid = get_odoo_server()
+    if server and uid:
+        partner_ids = server.execute_kw(
+            db,
+            uid,  # Convert uid to an integer
+            password,
+            "res.partner",
+            "search",
+            [[]],
+        )
+
+        if not partner_ids:
+            return "No partners found."
+        else:
+            partner_data = server.execute_kw(
+                db,
+                uid,  # Convert uid to an integer
+                password,
+                "res.partner",
+                "read",
+                [partner_ids],
+                {"fields": ["name", "is_company"]},
+            )
+            if partner_data:
+                partners_info = ""
+                for partner in partner_data:
+                    partners_info += f"ID: {partner['id']}<br>"
+                    partners_info += f"Name: {partner['name']}<br>"
+                    partners_info += f"Is Company: {partner['is_company']}<br>"
+                    partners_info += "-------<br>"
+                return partners_info
+            else:
+                return "Failed to retrieve partner data."
     else:
-        return "Hello World From Flask! Odoo is not connected."
+        return "Could not connect to Odoo server."
 
 
 if __name__ == "__main__":
